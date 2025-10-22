4115bda1b0b303538aca843726bf099f21698cd5
Janusch Patas <janusch.patas@gmx.de>
2025-10-22 19:50:15 +0200

    Revert "new tensor lib (#503)"
    This reverts commit 72d5b9531fb70b9a0a93563757b6aa60d5bfbfa1.

--- tests/test_geometry.cpp
+++ tests/test_geometry.cpp
@@ -278,3 +278,9 @@ TEST_F(EuclideanTransformTest, EulerAnglesRoundtrip) {
             << "Retrieved angles: (" << retrieved_angles.x << ", " << retrieved_angles.y << ", " << retrieved_angles.z << ")";
     }
 }
+
+// Main function for running tests
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
